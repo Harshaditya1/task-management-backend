479d67d463171c89ec4cb6290961e57c3dfbbd33
Harshaditya1 <patelboy90287484@gmail.com>
2026-07-15 08:37:16 +0530

    Create TaskServise

---
+++ src/main/java/com/harsh/taskmanagement/service/TaskService.java
@@ -0,0 +1,25 @@
+package com.harsh.taskmanagement.service;
+
+import com.harsh.taskmanagement.entity.Task;
+import com.harsh.taskmanagement.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
